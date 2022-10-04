68c61d11769f1427ed9b036c02e3d202f8ec8d11
unknown <floidslave@mail.ru>
2022-10-05 02:06:38 +0300

    finish code

--- slau.py
+++ slau.py
@@ -1,9 +1,11 @@
 A = []
+print("Введите длину массива")
 n = int(input())
+print("Введите строки")
 for i in range(n):
     A.append((str(input())))
 B = []
 for i in A:
-    if len(i) <= 3:
+    if len(i) <= 3:         #Условие задачи, что длина меньше или равна 3 символам
         B.append(i)
 print(B)
